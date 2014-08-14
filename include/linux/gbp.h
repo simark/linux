6497ec5f4ac7d8514b8eca64648c193822c609fa
Simon Marchi <simon.marchi@ericsson.com>
2014-08-14 14:31:07 -0400

    Add some comments

--- include/linux/gbp.h
+++ include/linux/gbp.h
@@ -4,14 +4,18 @@
 
 struct gbp_session {
 	struct mutex mutex;
+	/* List of breakpoints associated to this session. */
 	struct list_head bp_list;
 	unsigned long bp_ids;
 
+	/* Tasks blocked on a breakpoint of this session. */
 	unsigned long bp_slots;
 	struct task_struct *blocked_task[GBP_MAX_ENTRY_SLOTS];
+
 	spinlock_t entry_lock;
 	wait_queue_head_t waitq;
 
+	/* Node for the list of sessions. */
 	struct list_head node;
 };
 
@@ -27,9 +31,13 @@ struct gbp_bp {
 	struct gbp_session *gbp_s;
 	unsigned int bp_id;
 
+	/* File and offset of the breakpoint */
 	struct inode *inode;
 	unsigned long offset;
+
+	/* Node for list of breakpoints in a session. */
 	struct list_head node;
 };
 
+/* Global list of sessions */
 extern struct list_head gbp_sessions;
